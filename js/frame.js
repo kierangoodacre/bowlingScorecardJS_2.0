7fdbe77759ef5da5a9ca0c81b6d1fadb0ee2ea96
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-28 16:21:36 +0000

    created frame object with score

---
+++ js/frame.js
@@ -0,0 +1,10 @@
+var Frame = function (){
+	this.score;
+	this.throwCount = 2;
+};
+
+Frame.prototype.rackScore = function (pinsKnockedDown){
+	this.score = pinsKnockedDown;
+	this.throwCount -= 1;
+};
+
---
+++ spec/frameSpec.js
@@ -0,0 +1,28 @@
+describe ("Frame", function(){
+
+	var frame;
+
+	beforeEach(function(){
+		frame = new Frame();
+	});
+
+	describe("Score", function(){
+
+		it("Pins knocked down with throw equates to points gained", function(){
+			frame.rackScore(10);
+			expect(frame.score).toEqual(10);
+		});
+
+	});
+
+	describe("Throws", function(){
+
+		it("Has a max of two throws in one frame ", function(){
+			expect(frame.throwCount).toEqual(2);
+			frame.rackScore(8);
+			expect(frame.throwCount).toEqual(1);
+		});
+
+	}); 
+
+});
